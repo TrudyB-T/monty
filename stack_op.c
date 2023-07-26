3cda926867538cfa12eae015609313d2d199b70f
Im-adom <graceamaningkwarteng@gmail.com>
2023-07-26 12:18:36 -0700

    Sets the format to stack

---
+++ stack_op.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * f_stack - This sets the format of the data to a stack.
+ * @head: the head of the stack.
+ * @counter: iterator
+ */
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	doc.lifi = 0;
+}
