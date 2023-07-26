73e0517331639acf5e92c8c57e347070d1b8432b
Im-adom <graceamaningkwarteng@gmail.com>
2023-07-26 12:22:45 -0700

    This adds nothing to the stack

---
+++ nop_op.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * f_nop - This opcode does nothing.
+ * @head: the stack of the head.
+ * @counter: iterator
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
